9aecc1fa1b5acfdc8f491ef7c80ea74e344658cf
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-04 10:20:05 -0600

    Update ODM.cs

--- RS4A/Items/ODM.cs
+++ RS4A/Items/ODM.cs
@@ -5,21 +5,21 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace YourModName.Items
+namespace RS4A.Items
 {
     public class ODM : ModItem
     {
         public override void SetDefaults()
         {
             //clone and modify the ones we want to copy
-            item.CloneDefaults(ItemID.DiamondHook);
-            item.name = "ODM gear";
+            item.CloneDefaults(ItemID.AmethystHook);
+           // item.displayName = "ODM gear";
             item.shootSpeed = 18f; // how quickly the hook is shot.
-            item.shoot = mod.ProjectileType("OD");
+            item.shoot = mod.ProjectileType("ODM");
         }
         public override void AddRecipes()  //How to craft this item
         {
-            /*
+         /*   
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.Wood, 10);   //you need 10 Wood
             recipe.AddIngredient(null, "ItemName");  //an example of how to add your custom ingredient
