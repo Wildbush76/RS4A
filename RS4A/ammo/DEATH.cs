1fafcdcd588d8955bd6f502c5821d6ba62f9e3bf
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-05 15:40:48 -0600

    Update DEATH.cs

--- RS4A/ammo/DEATH.cs
+++ RS4A/ammo/DEATH.cs
@@ -1,1 +1,32 @@
+public class Ammo : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("DEATH");
+		}
+		public override void SetDefaults()
+		{
+			item.damage = 10000;
+			item.ranged = true;
+			item.width = 7;
+			item.height = 13;
+			item.maxStack = 999;
+			item.consumable = true;
+			item.knockBack = 1.2f;
+			item.rare = ItemRarityID.Blue;
+			item.shoot = mod.ProjectileType("dyno_arrow");
+			item.shootSpeed = 8.5f;
+			item.ammo = AmmoID.Arrow;
+			
 
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.WoodenArrow,10000);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this,1);
+			recipe.AddRecipe();
+
+		}
+	}
