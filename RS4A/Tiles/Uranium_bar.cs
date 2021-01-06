7ce2dff0fccb10386ee00f151001eaf132ebb515
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-06 13:02:30 -0600

    Create Uranium_bar.cs

---
+++ RS4A/Tiles/Uranium_bar.cs
@@ -0,0 +1,19 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RS4A.Items
+{
+
+public class Uranium_bar
+{
+public override void SetStaticDefaults()
+{
+
+}
+}
+
+}
