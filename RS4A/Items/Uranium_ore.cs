c7632c6135f5efc0410fa580555d8908ff333ac2
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-06 12:52:53 -0600

    Create Uranium_ore.cs

---
+++ RS4A/Items/Uranium_ore.cs
@@ -0,0 +1,18 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RS4A.Items
+{
+   public class Uranium_ore : ModItem
+  {
+     public override void SetStaticDefualts()
+     {
+
+     }
+  }
+
+}
