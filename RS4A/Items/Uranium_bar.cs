b584f59afec3239bfd60f0d661660ddcc3c99c68
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-06 12:44:19 -0600

    Update Uranium_bar.cs

--- RS4A/Items/Uranium_bar.cs
+++ RS4A/Items/Uranium_bar.cs
@@ -1,1 +1,23 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace RS4A.Items
+{
+    public class Uranium_bar : ModItem
+    {
+       public override void setStacticDefaults()
+       {
+        DisplayName.SetDefault("Uranium Bar");
+			  Tooltip.SetDefault("Tastes good");
+       }
+       public override void setDefaults()
+       {
+       
+       }
+    
+    }
+}
