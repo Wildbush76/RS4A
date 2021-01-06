4a2c676617b1d80cfecd52df63fa10a00a289ded
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-06 17:52:03 -0600

    Update Uranium_bar.cs

--- RS4A/Items/Uranium_bar.cs
+++ RS4A/Items/Uranium_bar.cs
@@ -9,20 +9,22 @@ namespace RS4A.Items
 {
     public class Uranium_bar : ModItem
     {
-       public override void setStacticDefaults()
-       {
-        DisplayName.SetDefault("Uranium Bar");
-			  Tooltip.SetDefault("Tastes good");
-       }
-       public override void setDefaults()
-       {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Uranium Bar");
+            Tooltip.SetDefault("Tastes good");
+        }
+        public override void SetDefaults()
+        {
+            item.maxStack = 99; 
+        }
+      
        
-       }
        public override void AddRecipes()
-	{
+	   {
 	ModRecipe recipe = new ModRecipe(mod);
-	recipe.AddIngredient(null,"Uranium_ore",3);
-	recipe.AddTile(TileID.MythrilAnvil);
+	recipe.AddIngredient(null,"Uranium_or",3);
+	recipe.AddTile(TileID.AdamantiteForge);
 	recipe.SetResult(this,1);
 	recipe.AddRecipe();
        }
