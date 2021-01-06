bd8009185872c0e751d84f847470134746b7def0
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-06 13:06:28 -0600

    Update Uranium.cs

--- RS4A/Tiles/Uranium.cs
+++ RS4A/Tiles/Uranium.cs
@@ -1,1 +1,17 @@
-//ill add later but new ore comin
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RS4A.Items
+{
+public class Uranium_ore
+{
+public override void SetStaticDefaults()
+{
+
+}
+}
+}
