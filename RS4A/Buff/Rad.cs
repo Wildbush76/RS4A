c24517b89dde38f9fc7498b193e3925e56ee2130
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-07 08:35:12 -0600

    Update Rad.cs

--- RS4A/Buff/Rad.cs
+++ RS4A/Buff/Rad.cs
@@ -1,1 +1,14 @@
+using Microsoft.Xna.Framework;
+u
+}sing Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace RS4A.Items
+{
+    public class Nfuel : ModItem
+    {
+    }
+}
