e44f1bc26a2f5fbce7e33df9e11fbcbcbaaf2bdf
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-06 20:26:24 -0600

    Update Nukep.cs

--- RS4A/Projectiles/Nukep.cs
+++ RS4A/Projectiles/Nukep.cs
@@ -7,12 +7,31 @@ using Terraria.ModLoader;
 
 namespace RS4A.Projectiles
 {
-    public class Nukep : ModProjectile
-    {
-        public override void SetDefaults()
-        {
-           // code nuke here
-        }
-        
-    }
-}
+	public class dyno_arrow : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+
+			DisplayName.SetDefault("Dynamite arrows");
+
+		}
+		public override void SetDefaults()
+		{
+			projectile.damage = 30;
+			projectile.friendly = true;
+			projectile.ranged = true;
+			projectile.width = 14;
+			projectile.height = 32;
+			projectile.aiStyle = 34;
+			projectile.penetrate = 1;
+			projectile.arrow = true;
+
+		}
+		public override void Kill(int timeLeft)
+		{
+			
+		}
+
+
+
+	}
