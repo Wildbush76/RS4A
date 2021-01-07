0ceb26e9618bbd441cc4d2003fdf7af0c4bb1c55
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-07 13:55:23 -0600

    Update Nukep.cs

--- RS4A/Projectiles/Nukep.cs
+++ RS4A/Projectiles/Nukep.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-
 namespace RS4A.Projectiles
 {
 	public class Nukep : ModProjectile
@@ -18,21 +17,20 @@ namespace RS4A.Projectiles
 		public override void SetDefaults()
 		{
 			projectile.damage = 150;
-			projectile.friendly = true;
+			projectile.friendly = false;
 			projectile.ranged = true;
 			projectile.width = 14;
 			projectile.height = 32;
 			projectile.aiStyle = 34;
 			projectile.penetrate = 1;
-			//projectile.rocket = true; //too lazy to figure out rn
-
+			
 		}
 		public override void Kill(int timeLeft)
 		{
 			Vector2 position = projectile.Center;
 				Main.PlaySound(SoundID.Item14, (int)position.X, (int)position.Y);
 			   
-				int radius = 10;     //this is the explosion radius, the highter is the value the bigger is the explosion
+				int radius = 10;
 
 				for (int x = -radius; x <= radius; x++)
 				{
@@ -43,7 +41,7 @@ namespace RS4A.Projectiles
 
 						if (Math.Sqrt(x * x + y * y) <= radius + 0.5)   //this make so the explosion radius is a circle
 						{
-						
+											
 							WorldGen.KillTile(xPosition, yPosition, false, false, false);  //this make the explosion destroy tiles  
 							Dust.NewDust(position, 22, 22, DustID.Smoke, 0.0f, 0.0f, 120, new Color(), 1f);  //this is the dust that will spawn after the explosion
 						}
