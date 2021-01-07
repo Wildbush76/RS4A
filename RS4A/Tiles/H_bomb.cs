aa97c802819b7a2d7743548109330fb82f95b927
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-07 09:35:28 -0600

    Create H_bomb.cs

---
+++ RS4A/Tiles/H_bomb.cs
@@ -0,0 +1,35 @@
+
+ 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RS4A.Tiles
+{
+public class H_bomb : ModTile
+{
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileLavaDeath[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            Main.tileSpelunker[Type] = true;
+
+            drop = mod.ItemType("Uranium_or");
+
+            soundType = SoundID.Tink;
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Hydrogen Bomb");
+            AddMapEntry(new Color(255,125,0),name);
+
+            minPick = 50;
+            mineResist = 10f;
+
+        }
+
+    }
+}
