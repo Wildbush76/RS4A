bea399ad62a2ffcf1fa9beebeadba01db2b40409
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-07 12:33:58 -0600

    Update H_bomb.cs

--- RS4A/Tiles/H_bomb.cs
+++ RS4A/Tiles/H_bomb.cs
@@ -29,6 +29,34 @@ public class H_bomb : ModTile
             mineResist = 10f;
 
         }
+        
+        /*
+        int radius = 10;
+        while(radius < 199)
+        {
+        Vector2 position = projectile.Center;
+				Main.PlaySound(SoundID.Item14, (int)position.X, (int)position.Y);
+			   
+				
+				for (int x = -radius; x <= radius; x++)
+				{
+					for (int y = -radius; y <= radius; y++)
+					{
+						int xPosition = (int)(x + position.X / 16.0f);
+						int yPosition = (int)(y + position.Y / 16.0f);
+
+						if (Math.Sqrt(x * x + y * y) <= radius + 0.5)   //this make so the explosion radius is a circle
+						{
+						
+							WorldGen.KillTile(xPosition, yPosition, false, false, false);  //this make the explosion destroy tiles  
+							Dust.NewDust(position, 22, 22, DustID.Smoke, 0.0f, 0.0f, 120, new Color(), 1f);  //this is the dust that will spawn after the explosion
+						}
+					}
+        		}
+                        radius++;
+                        Threading.sleep(500);
+        }
+        */
 
     }
 }
