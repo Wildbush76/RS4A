bf6eb15dae3beb889d26e8bfeea12c7c4d0799b3
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-06 19:24:48 -0600

    Update Nukep.cs

--- RS4A/Projectiles/Nukep.cs
+++ RS4A/Projectiles/Nukep.cs
@@ -1,1 +1,18 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
+namespace RS4A.Projectiles
+{
+    public class Nukep : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+           // code nuke here
+        }
+        
+    }
+}
