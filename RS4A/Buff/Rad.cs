3d3bf8fe8702a84c614dcf51159d428de18a7961
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-01-07 20:29:02 -0600

    Update Rad.cs

--- RS4A/Buff/Rad.cs
+++ RS4A/Buff/Rad.cs
@@ -7,9 +7,21 @@ using Terraria.ModLoader;
 
 namespace RS4A.Buffs
 {
-    public class Nfuel : ModBuffs
+    public class Rad : ModBuff
     {
-    // idk how tf to do this
-    //ill add stuf later for now it does nothing
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Radatiom");
+            Description.SetDefault("Radation is not fun, lose much heath you do");
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.poisoned = true;
+            player.lifeRegen -= 200;
+            player.moveSpeed -= .7f;
+            player.allDamage += 80;
+        }
+        // idk how tf to do this
+        //ill add stuf later for now it does nothing
     }
 }
