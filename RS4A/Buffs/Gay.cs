50b7774979e7d5c7aa13e2af19a8c6b2a0c9f84a
Wildbush76 <75509166+Wildbush76@users.noreply.github.com>
2021-02-03 12:53:41 -0600

    Create Gay.cs

---
+++ RS4A/Buffs/Gay.cs
@@ -0,0 +1,23 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RS4A.Buffs
+{
+    public class Rad : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Gay");
+            Description.SetDefault("your gay now");
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+        
+        }
+        
+    }
+}
