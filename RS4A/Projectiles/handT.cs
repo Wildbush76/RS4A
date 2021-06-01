8c72064de4343158c200ae9e4648461dc79aa4af
kakaeater33 <77019279+kakaeater33@users.noreply.github.com>
2021-05-31 23:04:46 -0500

    Add files via upload

---
+++ RS4A/Projectiles/handT.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace death.Projectiles
+{
+	public class handT : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+
+			DisplayName.SetDefault("hand");
+
+		}
+		public override void SetDefaults()
+		{
+			projectile.scale = 1;
+			projectile.damage = 30;
+			projectile.friendly = true;
+			projectile.ranged = true;
+			projectile.width = 30;
+			projectile.height = 24;
+			projectile.aiStyle = 2;
+			projectile.penetrate = 9999;
+			projectile.tileCollide = true;
+			projectile.velocity *= 1.9f;
+		}
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			target.defense = 0;
+			target.AddBuff(BuffID.Venom, 240);
+			Main.PlaySound(2, projectile.position, 14);
+
+		}
+		public override void Kill(int timeLeft)
+		{
+			Collision.HitTiles(projectile.position, projectile.velocity, projectile.width, projectile.height);
+		}
+	}
+}
