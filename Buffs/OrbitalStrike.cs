e867a14017115a90eae60a857fc6cadbbfd2f53f
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2023-11-09 09:25:14 -0600

    adding orbital strike debuff

---
+++ Buffs/OrbitalStrike.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace RS4A.Buffs
+{
+    internal class OrbitalStrike : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = false;
+            BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
+        }
+    }
+}
--- Detours/wormHoleDetour.cs
+++ Detours/wormHoleDetour.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Steamworks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
@@ -15,13 +17,44 @@ namespace RS4A.Detours
         public override void Load() {
             On_Player.HasUnityPotion += On_Player_HasUnityPotion;
             On_Player.TakeUnityPotion += On_Player_TakeUnityPotion;
+            On_Player.UnityTeleport += On_Player_UnityTeleport;
+        }
+
+        private void On_Player_UnityTeleport(On_Player.orig_UnityTeleport orig, Player self, Microsoft.Xna.Framework.Vector2 telePos)
+        {
+            if (self.HeldItem.type.Equals(ModContent.ItemType<Items.Nuke>()))
+            {
+                //find the player
+                double closest = double.MaxValue;
+                Player closestPlayer = null;
+                for (int i = 0; i < Main.maxPlayers; i++) {
+                    Player player = Main.player[i];
+                    if (player.active) {
+                        double dist = Vector2.Distance(new Vector2(telePos.X,telePos.Y), new Vector2(player.Center.X, player.Center.Y));
+                        if (dist < closest) {
+                            closest = dist;
+                            closestPlayer = player;
+                        }
+                    }
+                }
+                if (closestPlayer == null)
+                {
+                    return;
+                }
+                else
+                {
+                    closestPlayer.AddBuff(ModContent.BuffType<Buffs.OrbitalStrike>(), 5);
+                }
+            }
+            else {
+                orig(self,telePos);
+            }
         }
 
         private void On_Player_TakeUnityPotion(On_Player.orig_TakeUnityPotion orig, Player self)
         {
             if (self.HeldItem.type.Equals(ModContent.ItemType<Items.Nuke>()))
             {
-                self.QuickSpawnItem(self.GetSource_FromAI(), 200);
                 return;
             }
             else
@@ -32,7 +65,13 @@ namespace RS4A.Detours
 
         private bool On_Player_HasUnityPotion(On_Player.orig_HasUnityPotion orig, Player self)
         {
-            return self.HeldItem.type.Equals(ModContent.ItemType<Items.Nuke>());
+            if (self.HeldItem.type.Equals(ModContent.ItemType<Items.Nuke>()))
+            {
+                return true;
+            }
+            else {
+                return orig(self);
+            }
         }
     }
 }
