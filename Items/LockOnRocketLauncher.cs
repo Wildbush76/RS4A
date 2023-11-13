017f5a8a7b505a01ca2b42cd1d5d9c3502c48ed4
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2023-11-12 21:04:41 -0600

    setup for maybe rocket luancher item

---
+++ Items/LockOnRocketLauncher.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RS4A.Items
+{
+    internal class LockOnRocketLauncher : ModItem
+    {
+        private Projectile targeting;
+        public override void SetDefaults()
+        {
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useAmmo = AmmoID.Rocket;
+            Item.rare = ItemRarityID.Red;
+            Item.useTime = 80;
+            Item.useAnimation = 80;
+            Item.channel = true;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if (targeting != null)
+            {
+                //something
+            }
+            else
+            {
+                targeting = Projectile.NewProjectileDirect(player.GetSource_FromThis(),player.position,Vector2.Zero,1,0,0);
+            }
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if(player.whoAmI != Main.myPlayer) {
+                return null; 
+            }
+
+            //set target tracking
+            return null;
+        }
+
+    }
+}
---
+++ Projectiles/RocketLauncherTargeting.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RS4A.Projectiles
+{
+    internal class RocketLauncherTargeting : ModProjectile
+    {
+        private readonly bool lockedOn;
+        private NPC lockedOnNPC;
+        private const double lockOnRadius = 5 * 8;
+
+        public override void SetDefaults()
+        {
+            Projectile.tileCollide = false;
+
+        }
+
+        public override void AI()
+        {
+            if (Main.player[Projectile.owner].HeldItem.type == ModContent.ItemType<Items.LockOnRocketLauncher>()) {
+                Projectile.timeLeft = 0;
+            }
+            if (lockedOn)
+            {
+               
+            }
+            else
+            {
+                Projectile.position = Main.MouseWorld;
+
+            }
+        }
+
+        private NPC findClosestNPC()
+        {
+            NPC closest = null;
+            float closestDist = float.MaxValue;
+            for (int k = 0; k < Main.maxNPCs; k++)
+            {
+                NPC target = Main.npc[k];
+                if (target.CanBeChasedBy())
+                {
+                    float dist = Vector2.Distance(target.position,Projectile.position);
+                    if (dist < closestDist) {
+                        closest = target;
+                        closestDist = dist;
+                    }
+                }
+            }
+            return closest;
+        }
+
+    }
+}
