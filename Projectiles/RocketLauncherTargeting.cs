d015b525c1c8e8e61fcd502571a2f0295ad7d119
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2023-11-13 23:07:52 -0600

    adding lock on delay

--- Projectiles/RocketLauncherTargeting.cs
+++ Projectiles/RocketLauncherTargeting.cs
@@ -9,7 +9,9 @@ namespace RS4A.Projectiles
     {
         private bool lockedOn = false;
         private NPC lockedOnNPC;
-        private const double lockOnRadius = 5 * 8;
+        private const double lockOnRadius = 48;
+        private int lockOnTimer = 0;
+        private const int minLockOnTime = 120;
 
         public override void SetDefaults()
         {
@@ -28,12 +30,13 @@ namespace RS4A.Projectiles
 
         public override void AI()
         {
-            if (Main.player[Projectile.owner].HeldItem.type != ModContent.ItemType<Items.LockOnRocketLauncher>()) {
+            if (Main.player[Projectile.owner].HeldItem.type != ModContent.ItemType<Items.LockOnRocketLauncher>()) {//make sure they are holding the item
                 Projectile.timeLeft = 0;
             }
+
             if (lockedOn)
             {
-                Projectile.Center = lockedOnNPC.Center;
+                Projectile.Center = lockedOnNPC.Center;//maybe change this to have it chase them?
             }
             else
             {
@@ -42,12 +45,23 @@ namespace RS4A.Projectiles
                 if (closest == null) {
                     return;
                 }
-                if (Vector2.Distance(closest.Center, Projectile.Center) < lockOnRadius) {
-                    lockedOn = true;
-                    lockedOnNPC = closest;
+                if (Vector2.Distance(closest.Center, Projectile.Center) < lockOnRadius)
+                {
+                    if (lockOnTimer != minLockOnTime)
+                    {
+                        lockOnTimer++;
+                    }
+                    else
+                    {
+                        lockedOn = true;
+                        lockedOnNPC = closest;
+                    }
+                }
+                else {
+                    lockOnTimer = 0;
                 }
             }
-            Projectile.rotation= MathHelper.ToRadians(6);
+            Projectile.rotation += MathHelper.ToRadians(3);
         }
 
         private NPC FindClosestNPC()
