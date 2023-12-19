f2098bda759dd7519f2d90aa8131f0f834d671ab
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2023-12-19 13:44:58 -0600

    upading hallowed bullets

--- Projectiles/HallowedBullet.cs
+++ Projectiles/HallowedBullet.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -9,8 +10,9 @@ namespace RS4A.Projectiles
     {
         private const int outerRange = 80;
         private const int innerRange = 20;
-        private const int maxVelocity = 30;
-        public override string Texture => "Terraria/Images/Projectiles_" + ProjectileID.ChlorophyteBullet;
+        private const int maxVelocity = 60;
+        private const int maxNormalVelocity = 30;
+        public override string Texture => "Terraria/Images/Item_" + ItemID.ChlorophyteBullet;
         public override void SetDefaults()
         {
             Projectile.damage = 30;
@@ -19,28 +21,37 @@ namespace RS4A.Projectiles
             Projectile.penetrate = 3;
         }
 
+
+        private float GetDistance(Vector2 posOne, int width, int height, Vector2 posTwo) { 
+            float maxOffset = Math.Max(width, height);
+            return Vector2.Distance(posOne,posTwo) + maxOffset;
+        }
+        
+
         public override void AI()
         {
             NPC closestNpc = null;
+            float dist = 0;
             for (int i = 0; i < Main.maxNPCs; i++)
             {
                 NPC currentNPC = Main.npc[i];
-                if (currentNPC != null && !currentNPC.CanBeChasedBy() && Vector2.Distance(Projectile.Center, currentNPC.Center) < outerRange)
+                float tempDist = GetDistance(currentNPC.Center, currentNPC.width, currentNPC.height, Projectile.Center);
+                if (currentNPC != null && !currentNPC.CanBeChasedBy() && tempDist < outerRange)
                 {
                     closestNpc = currentNPC;
-
+                    dist = tempDist;
                 }
             }
             if (closestNpc is null)
             {
-                if (Projectile.velocity.Length() > maxVelocity)
-                {
-                    Projectile.velocity = Vector2.Normalize(Projectile.velocity) * maxVelocity;
+                if (Projectile.velocity.Length() > maxNormalVelocity) { 
+                    Projectile.velocity = Vector2.Normalize(Projectile.velocity) * maxNormalVelocity;
                 }
                 return;
             }
-            float distance = Vector2.Distance(Projectile.Center, closestNpc.Center);
-            Projectile.velocity += (Projectile.Center - closestNpc.Center) * (innerRange / distance);
+            Vector2 target = Vector2.Normalize(Projectile.Center - closestNpc.Center) * (maxVelocity * (innerRange/dist));
+
+            Projectile.velocity = Vector2.Lerp(Projectile.velocity,target,innerRange/dist);
         }
     }
 }
