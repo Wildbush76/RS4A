3907c309f2e6f53429c21b40d6ffa50575af4f40
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2024-05-01 16:12:22 -0500

    getting the map drawing to work

--- PlayerStuff/MissileMapLayer.cs
+++ PlayerStuff/MissileMapLayer.cs
@@ -6,6 +6,7 @@ using Terraria.GameContent;
 using Terraria.Map;
 using Terraria.ModLoader;
 using Terraria.UI;
+using Terraria.DataStructures;
 
 namespace RS4A.PlayerStuff
 {
@@ -19,7 +20,8 @@ namespace RS4A.PlayerStuff
                 if (projectile.type == ModContent.ProjectileType<MissileProjectile>() && projectile.owner == Main.myPlayer)
                 {
                     Vector2 position = projectile.position / 16;
-                    context.Draw(TextureAssets.Projectile[ModContent.ProjectileType<MissileProjectile>()].Value, position, Alignment.Center);
+                    //context.Draw(TextureAssets.Projectile[ModContent.ProjectileType<MissileProjectile>()].Value, position, Alignment.Center);
+                    context.Draw(null, position, Color.Red, new SpriteFrame(1,1,0,0),0.3f,0.7f,Alignment.Center);
                 }
             }
         }
